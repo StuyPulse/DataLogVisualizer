b8977533ffa325ebd225b6d295e74ca227a084c6
Myles Pasetsky <mp3blue99@gmail.com>
2022-05-10 10:58:15 -0400

    Create Entries class,support plots with many keys

--- main.py
+++ main.py
@@ -2,56 +2,66 @@ import csv
 import sys
 from typing import Iterable
 
-from data import Data
+from data import Data, Value
 
 import matplotlib.pyplot as plt
 import numpy as np
 
-# class Entries:
-#     def __init__(self):
-#         self.table = {}
-Entries = dict[str, Data]
+class Entries:
+    def __init__(self) -> None:
+        self.table = {}
+    
+    def add(self, key: str, data: Data) -> None:
+        if key not in self.table:
+            self.table[key] = []
+        self.table[key].append(data) # assumes calls to add are sorted by time
+    
+    def get_value(self, key: str, time: float) -> Value:
+        # could binary search if calls to add are sorted by time
+        last = None
+        for point in self.table[key]:
+            if (last is None or time > last.time) and time < point.time:
+                return point.value
+            last = point
+        return self.table[key][-1].value
 
-def get_value_at_time(time: float, points: list[Data]):
-    last = None # TODO: bin search
-    for point in points:
-        if (last is None or time > last.time) and time < point.time:
-            return point.value
-        last = point
+    def get_values(self, key: str) -> list[Data]:
+        return self.table[key]
+    
+    def get_timespan(self, key: str) -> tuple[float, float]:
+        values = self.get_values(key)
+        return values[0].time, values[-1].time
 
-    return points[-1].value
+    def get_timeseries(self, key: str, t = None):
+        if t is None:
+            t = np.arange(0, data.get_timespan(key)[1], 0.02)
+        v = np.zeros(t.size, dtype=t.dtype)
 
+        for index, time in enumerate(t): 
+            v[index] = float(data.get_value(key, time)) # converts bools into floats
+
+        return t, v
 
 def get_data(file: Iterable[str]) -> Entries:
     reader = csv.reader(file)
     next(reader) # skip first line as header, can also check if each line is equal
 
-    data = {}
+    data = Entries()
     for values in reader:
         time, key, value = values
-        
-        if key not in data:
-            data[key] = []
-        
-        data[key].append(Data(float(time), value))
+        data.add(key, Data(float(time), value))
 
     return data
 
-def plot_data(title: str, data: Entries, key: str): # TODO: be able to plot multiple keys
+def plot_data(title: str, data: Entries, keys: list[str]):     
     fig, ax = plt.subplots()
 
-    lasttime = data[key][-1].time
-    t = np.arange(0, lasttime, 0.02)
-    v = np.zeros((t.size), dtype=t.dtype)
-
-    for index, time in enumerate(t): 
-        v[index] = get_value_at_time(time, data[key])
-
-    ax.plot(t, v)
+    for key in keys:
+        ax.plot(*data.get_timeseries(key))
 
     ax.set(
         xlabel='time (s)',
-        ylabel=', '.join([key]),
+        ylabel=', '.join(keys),
         title=title
     )
     ax.grid()
@@ -66,7 +76,11 @@ if __name__ == "__main__":
         exit(1)
     
     data = get_data(infile)
-    plot_data(sys.argv[1], data, 'NT:/SmartDashboard/Shooter/Target RPM')
+    plot_data(sys.argv[1], data, [
+        'NT:/SmartDashboard/Shooter/Target RPM',
+        # 'NT:/limelight/tx',
+        # 'NT:/limelight/ty'
+    ])
 
     # for item in data.items():
     #     print(item)
