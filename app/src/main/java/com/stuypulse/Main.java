8c072af1a0b14cd95cb4378e2ead12d6e6cc9a16
BenG49 <64862590+BenG49@users.noreply.github.com>
2022-06-30 01:29:03 -0400

    Update StuyLib version

--- app/src/main/java/com/stuypulse/Main.java
+++ app/src/main/java/com/stuypulse/Main.java
@@ -4,6 +4,6 @@ import com.stuypulse.stuylib.streams.filters.IFilter;
 
 public class Main {
     public static void main(String[] args) {
-        IFilter f = IFilter.create(x -> x);
+        Plot plot = new Plot();
     }
 }
